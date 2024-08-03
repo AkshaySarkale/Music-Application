32d58188a5239b548f716edf276b48a5578c2691
Akshay Sarkale <akshaysarkale999@gmail.com>
2024-08-03 13:13:32 +0530

    Login Page Design

--- app/src/main/java/com/example/homepage/MainActivity.kt
+++ app/src/main/java/com/example/homepage/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.homepage
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        changeTheam()
         Toast.makeText(this,"This App In Under A Development Phase:)",Toast.LENGTH_LONG).show();
+    }
+    fun changeTheam()
+    {
+        val  theambtn=findViewById<Button>(R.id.theambtn);
+        theambtn.setOnClickListener {
+            Toast.makeText(this,"Checking For Updates",Toast.LENGTH_SHORT).show();
+        }
     }
 }
